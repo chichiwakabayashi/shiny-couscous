2865dd260449af63bd1cab8e62c3047517b7503f
chichiwakabayashi <eww2123@columbia.edu>
2023-05-08 17:07:11 -0400

    more

--- exercises/final/main.js
+++ exercises/final/main.js
@@ -1,13 +1,43 @@
-const canvas = document.querySelector("drawing-board"),
+const canvas = document.querySelector("canvas");
+toolBtns = document.querySelectorAll(".tool");
+sizeSlider = document.querySelector("#size-slider")
 ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+selectedTool = "brush";
+brushWidth = 3;
 
 window.addEventListener("load", () => {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
 });
 
-const drawing = (e) => {
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+const startDraw = (e) => {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.lineWidth = brushWidth;
 }
+
+const drawing = (e) => {
+    if(!isDrawing) return;
+
+    if(selectedTool === "brush"){
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+    }
+}
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(selectedTool);
+    });
+});
+
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
+
+canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", () => isDrawing = false);
